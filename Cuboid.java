a855aa704586853e94628555eae401fee6150c07
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2021-11-01 00:43:05 +0530

    Update Cuboid.java

--- Cuboid.java
+++ Cuboid.java
@@ -29,15 +29,16 @@ public class Cuboid
   public static void main(Strings[] args)
   {
     Scanner jk = new Scanner(System.in);
-    System.out.print("Input Length of Cuboid ");
+    System.out.print("Input Length of Cuboid: ");
     int l = jk.nextInt();
     System.out.println();
-    System.out.print("Input Breadth of Cuboid ");
+    System.out.print("Input Breadth of Cuboid: ");
     int b = jk.nextInt();
     System.out.println();
-    System.out.print("Input Height of Cuboid ");
+    System.out.print("Input Height of Cuboid: ");
     int h = jk.nextInt();
     System.out.println();
+    
     // Printing Surface Area and Volume
     Cuboid cuba = new Cuboid(2,3,4);
     System.out.println(cuba.surfaceArea());
